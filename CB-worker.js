166e50bd39690bdb7e9fad2c688ab32cfdc3f4e9
Code Boxx <code-boxx@mail.com>
2022-12-22 13:22:54 +0800

    22 Dec 2022
    Service worker update.

--- CB-worker.js
+++ CB-worker.js
@@ -1,22 +1,24 @@
-// (A) FILES TO CACHE
-const cName = "JSCalendar",
-cFiles = [
-  "assets/favicon.png",
-  "assets/icon-512.png",
-  "assets/HEAD-js-cal.jpg",
-  "assets/maticon.woff2",
-  "CB-manifest.json",
-  "assets/js-calendar.css",
-  "assets/js-calendar.js",
-  "js-calendar.html"
-];
-
-// (B) CREATE/INSTALL CACHE
-self.addEventListener("install", evt => evt.waitUntil(
-  caches.open(cName)
-  .then(cache => cache.addAll(cFiles))
-  .catch(err => console.error(err))
-));
+// (A) CREATE/INSTALL CACHE
+self.addEventListener("install", evt => {
+  self.skipWaiting();
+  evt.waitUntil(
+    caches.open("JSCalendar")
+    .then(cache => cache.addAll([
+      "assets/favicon.png",
+      "assets/icon-512.png",
+      "assets/HEAD-js-cal.jpg",
+      "assets/maticon.woff2",
+      "CB-manifest.json",
+      "assets/js-calendar.css",
+      "assets/js-calendar.js",
+      "js-calendar.html"
+    ]))
+    .catch(err => console.error(err))
+  );
+});
+ 
+// (B) CLAIM CONTROL INSTANTLY
+self.addEventListener("activate", evt => self.clients.claim());
 
 // (C) LOAD FROM CACHE FIRST, FALLBACK TO NETWORK IF NOT FOUND
 self.addEventListener("fetch", evt => evt.respondWith(
